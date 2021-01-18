afbfe031936508d01f6ddf3ed683931446deb3d3
taiwocoker <taiwofcoker@gmail.com>
2021-01-18 17:23:39 +0100

    Add logic to control the count

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from 'react'
+import data from './data'
 
 function App() {
- const [count, setCount] =useState(0)
- const [text,setText] = useState([]) 
-  
- const handleSubmit = (e) => {
-  e.preventDefault()
- }
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(count)
+    if(count <= 0) {
+      amount = 1
+    }
+    if (count > 0) {
+      amount = 8
+    }
+    setText(data.slice(0,amount))
+  }
   return (
     <section className='section-center'>
       <h3>Tired of boring lorem ipsum?</h3>
@@ -26,12 +34,13 @@ function App() {
           generate
         </button>
       </form>
-      <article className="lorem-text">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, consequuntur!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, ullam!</p>
+      <article className='lorem-text'>
+        {text.map((item,index) => {
+          return <p key={index}>{item}</p>
+        })}
       </article>
     </section>
   )
 }
 
-export default App;
+export default App
